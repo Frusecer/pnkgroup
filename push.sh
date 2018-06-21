11f685e2a52fcc107550b65a0a6cb90f5727a68a
Dmitry <fullhouseblack@gmail.com>
2018-06-21 23:27:32 +0300

    update ad

--- push.sh
+++ push.sh
@@ -6,9 +6,13 @@
 git config user.name Dmitry
 git config user.email fullhouseblack@gmail.com
 
-git add ad_08.12.17.xml > /dev/null 2>&1
-git commit -m "update ad" > /dev/null 2>&1
-git push -u origin master > /dev/null 2>&1
+# git add ad_08.12.17.xml > /dev/null 2>&1
+# git commit -m "update ad" > /dev/null 2>&1
+# git push -u origin master > /dev/null 2>&1
+
+git add ad_08.12.17.xml
+git commit -m "update ad"
+git push -u origin master
 
 
 #git add ad.xml
